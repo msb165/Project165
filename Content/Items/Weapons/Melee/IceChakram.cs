89ac12ec8b3ddb27031ba7d0410717594475bcd5
msb165 <75228465+msb165@users.noreply.github.com>
2023-12-22 23:11:57 -0300

    Add "Ice Chakram"

--- Content/Items/Weapons/Melee/IceChakram.cs
+++ Content/Items/Weapons/Melee/IceChakram.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Terraria;
 using Terraria.ModLoader;
+using Project165.Content.Projectiles.Melee;
+using Terraria.ID;
 
 namespace Project165.Content.Items.Weapons.Melee
 {
@@ -16,8 +14,17 @@ namespace Project165.Content.Items.Weapons.Melee
             Item.damage = 65;
             Item.knockBack = 8f;
             Item.useTime = 14;
-            Item.useAnimation = 18;
+            Item.useAnimation = 14;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item1;
             Item.DamageType = DamageClass.Melee;
+            Item.shoot = ModContent.ProjectileType<IceChakramProj>();
+            Item.shootSpeed = 14f;
+            Item.value = Item.buyPrice(0, 0, 40, 0);
+            Item.autoReuse = true;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.ResearchUnlockCount = 1;
         }
     }
 }
--- Content/Projectiles/Melee/IceChakramProj.cs
+++ Content/Projectiles/Melee/IceChakramProj.cs
@@ -1,13 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Project165.Content.Projectiles.Melee
 {
     public class IceChakramProj : ModProjectile
     {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 38;
+            Projectile.height = 38;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.aiStyle = -1;
+        }
+
+        private Player Owner => Main.player[Projectile.owner];
+        private readonly float returnSpeed = 18f;
+        private readonly float acceleration = 1.2f;
+
+        public override void AI()
+        {
+            Projectile.rotation += 0.25f * Owner.direction;
+            if (Projectile.ai[0] == 0f)
+            {
+                Projectile.ai[1] += 1f;
+                if (Projectile.ai[1] >= 30f)
+                {
+                    Projectile.ai[0] = 1f;
+                    Projectile.ai[1] = 0f;
+                    Projectile.netUpdate = true;
+                }
+            }
+            else
+            {                
+                Vector2 playerProjDistance = Owner.Center - Projectile.Center;
+                float distance = Vector2.Distance(Owner.Center, Projectile.Center);
+
+                if (distance > 3000f)
+                {
+                    Projectile.Kill();
+                }
+
+                distance = returnSpeed / distance;
+                playerProjDistance.X *= distance;
+                playerProjDistance.Y *= distance;
+                if (Projectile.velocity.X < playerProjDistance.X)
+                {
+                    Projectile.velocity.X += acceleration;
+                    if (Projectile.velocity.X < 0f && playerProjDistance.X > 0f)
+                    {
+                        Projectile.velocity.X += acceleration;
+                    }
+                }
+                else if (Projectile.velocity.X > playerProjDistance.X)
+                {
+                    Projectile.velocity.X -= acceleration;
+                    if (Projectile.velocity.X > 0f && playerProjDistance.X < 0f)
+                    {
+                        Projectile.velocity.X -= acceleration;
+                    }
+                }
+                if (Projectile.velocity.Y < playerProjDistance.Y)
+                {
+                    Projectile.velocity.Y += acceleration;
+                    if (Projectile.velocity.Y < 0f && playerProjDistance.Y > 0f)
+                    {
+                        Projectile.velocity.Y += acceleration;
+                    }
+                }
+                else if (Projectile.velocity.Y > playerProjDistance.Y)
+                {
+                    Projectile.velocity.Y -= acceleration;
+                    if (Projectile.velocity.Y > 0f && playerProjDistance.Y < 0f)
+                    {
+                        Projectile.velocity.Y -= acceleration;
+                    }
+                }
+                if (Main.myPlayer == Projectile.owner)
+                {
+                    if (Projectile.Hitbox.Intersects(Owner.Hitbox))
+                    {
+                        Projectile.Kill();
+                    }
+                }
+            }
+
+            GenerateDust();
+        }
+
+        private void GenerateDust()
+        {
+            Vector2 dustPosition = Projectile.Center; //+ Vector2.Normalize(Projectile.velocity) * 10f;
+            Vector2 dustVelocity = Projectile.velocity.RotatedBy(MathHelper.PiOver2);
+            Vector2 negativeDustVelocity = Projectile.velocity.RotatedBy(-MathHelper.PiOver2);
+
+            Dust iceDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.IceTorch);
+            iceDust.position = dustPosition;
+            iceDust.velocity = dustVelocity * 0.33f + Projectile.velocity / 4f;
+            iceDust.position += dustVelocity;
+            iceDust.fadeIn = 0.5f;
+            iceDust.noGravity = true;
+            Dust iceDust2 = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.IceTorch);
+            iceDust2.position = dustPosition;
+            iceDust2.velocity = negativeDustVelocity * 0.33f + Projectile.velocity / 4f;
+            iceDust2.position += negativeDustVelocity;
+            iceDust2.fadeIn = 0.5f;
+            iceDust2.noGravity = true;
+        }
     }
 }
