b9eaebdee946ca518d3d4280766f855f3476878e
msb165 <75228465+msb165@users.noreply.github.com>
2025-04-04 23:23:58 -0300

    Fix a typo

---
+++ Utilities/PathUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project165.Utilites;
+
+public partial class Project165Utils
+{
+    public const string AssetsPath = $"{nameof(Project165)}/Assets/";
+    public const string ContentPath = $"{nameof(Project165)}/Content/";
+    public const string ImagesPath = AssetsPath + "Images/";
+}
---
+++ Utilities/PlayerUtils.cs
@@ -0,0 +1,48 @@
+
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+
+namespace Project165.Utilites;
+
+public partial class Project165Utils
+{
+    public static void SmoothHoldStyle(Player player)
+    {
+        Vector2 direction = Vector2.Normalize(Main.MouseWorld - player.Center);
+        player.itemRotation = direction.ToRotation() * player.gravDir;
+        if (player.direction == -1)
+        {
+            player.itemRotation += MathHelper.Pi;
+        }
+        float rotation = player.itemRotation - MathHelper.PiOver2 * player.gravDir;
+        if (player.direction == -1)
+        {
+            rotation += MathHelper.Pi;
+        }
+        player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, rotation);
+        player.ChangeDir(MathF.Sign(direction.X));
+    }
+
+    public static void RecoilEffect(Player player, float intensity = 0)
+    {
+        float animProgress = 1f - player.itemAnimation / (float)player.itemAnimationMax;
+        float baseIntensity = 0.85f + intensity;
+        //player.itemRotation = (player.itemAnimation / (float)player.itemAnimationMax - 0.5f) * -player.direction * 3.5f - player.direction * 0.3f;
+        float recoil = MathHelper.Clamp(Utils.Remap(animProgress, 0f, 0.01f, 0f, 0.75f) * Utils.Remap(animProgress, 0.375f, 0.75f, 0.75f, 0f), 0f, 1f) * player.direction * baseIntensity;
+        Vector2 direction = Vector2.Normalize(Main.MouseWorld - player.Center);
+        player.itemRotation = direction.ToRotation() * player.gravDir - recoil;
+        if (player.direction == -1)
+        {
+            player.itemRotation += MathHelper.Pi;
+        }
+        float rotation = player.itemRotation - MathHelper.PiOver2 * player.gravDir;
+        if (player.direction == -1)
+        {
+            rotation += MathHelper.Pi;
+        }
+        player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, rotation);
+        player.ChangeDir(MathF.Sign(direction.X));
+
+    }
+}
